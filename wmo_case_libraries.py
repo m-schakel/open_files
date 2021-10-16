e556e69bb515c754bb46fa0fabda8e238f910cda
Michiel Schakel <michiel.schakel@gmail.com>
2021-10-16 16:37:14 +0200

    .

--- wmo_case_libraries.py
+++ wmo_case_libraries.py
@@ -1,11 +1,11 @@
 import requests
-import cbsodata 
-import pandas as pd                                         # Library to work with dataframes
-from string import punctuation                              # String manipulation
-import altair as alt                                        # Create interactive charts and maps
+import cbsodata
+import pandas as pd  # Library to work with dataframes
+from string import punctuation  # String manipulation
+import altair as alt  # Create interactive charts and maps
 
-import seaborn as sns                                       # especially for pairplots
-import numpy as np                                          # fill diagonal...corr plot
+import seaborn as sns  # especially for pairplots
+import numpy as np  # fill diagonal...corr plot
 import matplotlib.pyplot as plt
 
 import sklearn
@@ -13,77 +13,96 @@ from sklearn.model_selection import train_test_split, cross_validate, cross_val_
 from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, StandardScaler
 from sklearn.compose import make_column_transformer, ColumnTransformer
 from sklearn.impute import SimpleImputer
-from sklearn.pipeline import make_pipeline, Pipeline 
+from sklearn.pipeline import make_pipeline, Pipeline
 from sklearn.linear_model import LinearRegression, LassoCV, Lasso, RidgeCV
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error
 
 
-def strip_strings( df ):
-  df_obj = df.select_dtypes(['object'])
-  df[df_obj.columns] = df_obj.apply(lambda x: x.str.strip())
+def strip_strings(df):
+    df_obj = df.select_dtypes(["object"])
+    df[df_obj.columns] = df_obj.apply(lambda x: x.str.strip())
 
-  return(df)
+    return df
 
-def get_cbs_metadata( dataset ):
-  # Purpose: Getting and printing CBS metadata for a specific datasets (e.g. 84751NED)
-  #          Info and available datasets: https://opendata.cbs.nl/statline/portal.html?_la=nl
-  # Example of execution: check_columns( { 2019: '84753NED', 2020: '84908NED' } )
-  # Arguments:
-  #   dataset              = stringvalue that refers to the CBS dataset (e.g. '84908NED')
-  # Returns:
-  #   l_metadata           = dataframe with metadata of the given dataset. Some details are printed as well.
 
-  # Downloading table list
-  #toc = pd.DataFrame(cbsodata.get_table_list())
+def get_cbs_metadata(dataset):
+    # Purpose: Getting and printing CBS metadata for a specific datasets (e.g. 84751NED)
+    #          Info and available datasets: https://opendata.cbs.nl/statline/portal.html?_la=nl
+    # Example of execution: check_columns( { 2019: '84753NED', 2020: '84908NED' } )
+    # Arguments:
+    #   dataset              = stringvalue that refers to the CBS dataset (e.g. '84908NED')
+    # Returns:
+    #   l_metadata           = dataframe with metadata of the given dataset. Some details are printed as well.
 
-  # Downloading metadata
-  l_metadata = pd.DataFrame(cbsodata.get_meta(dataset, 'DataProperties'))
-  print(l_metadata[['Key','Title','Type']])
+    # Downloading table list
+    # toc = pd.DataFrame(cbsodata.get_table_list())
 
-  return( l_metadata )
+    # Downloading metadata
+    l_metadata = pd.DataFrame(cbsodata.get_meta(dataset, "DataProperties"))
+    print(l_metadata[["Key", "Title", "Type"]])
 
-def get_cbs_data( datasets, filters, select ):
-  # Purpose: Getting CBS data for several similar datasets (e.g. same dataset over several years)
-  # Example of execution: check_columns( { 2019: '84753NED', 2020: '84908NED' } )
-  # Arguments:
-  #   datasets  = dictionary with keys = year and values = dataset
-  #   filters   = filter provided to odata-request
-  #   select    = dictionary with keys = CBS-columns to select, values = new name of columns
-  # Returns:
-  #   df_total  = dataframe with CBS over multiple years.
+    return l_metadata
 
-  list_of_df = []
 
-  select_list = [ column_o for (column_o,column_n) in select.items() ]
-  rename_list = ['year'] + [ column_n for (column_o,column_n) in select.items() ]
+def get_cbs_data(datasets, filters, select):
+    # Purpose: Getting CBS data for several similar datasets (e.g. same dataset over several years)
+    # Example of execution: check_columns( { 2019: '84753NED', 2020: '84908NED' } )
+    # Arguments:
+    #   datasets  = dictionary with keys = year and values = dataset
+    #   filters   = filter provided to odata-request
+    #   select    = dictionary with keys = CBS-columns to select, values = new name of columns
+    # Returns:
+    #   df_total  = dataframe with CBS over multiple years.
 
-  for (year,dataset) in datasets.items():
-    print( f'Retrieving CBS-data - year: {year} (dataset: {dataset})...', end='' )
+    list_of_df = []
 
-    df = pd.DataFrame(
-        cbsodata.get_data(dataset, 
-                          filters=filters.format(year),
-                          select=select_list)) 
+    select_list = [column_o for (column_o, column_n) in select.items()]
+    rename_list = ["year"] + \
+        [column_n for (column_o, column_n) in select.items()]
 
-    # Add year to the dataframe as first column
-    df.insert(0, 'year', year)
+    for (year, dataset) in datasets.items():
+        print(f"Retrieving CBS-data - year: {year} (dataset: {dataset})...",
+              end="")
 
-    print( f'  ready - size: {df.shape}')
+        df = pd.DataFrame(
+            cbsodata.get_data(dataset,
+                              filters=filters.format(year),
+                              select=select_list))
 
-    # Add dataframe to list and release memory
-    list_of_df.append ( df )
-    del df
+        # Add year to the dataframe as first column
+        df.insert(0, "year", year)
 
-  print( 'Concatenate...')
-  df_total = pd.concat(list_of_df, ignore_index=True)
+        print(f"  ready - size: {df.shape}")
 
-  print( 'Rename columns...')
-  df_total.columns = rename_list
+        # Add dataframe to list and release memory
+        list_of_df.append(df)
+        del df
 
-  print( 'Remove all leading and trailing spaces...')
-  df_total = strip_strings(df_total)
-  
-  print( f'Ready, size: {df_total.shape}' )
+    print("Concatenate...")
+    df_total = pd.concat(list_of_df, ignore_index=True)
 
-  return( df_total ) 
+    print("Rename columns...")
+    df_total.columns = rename_list
+
+    print("Remove all leading and trailing spaces...")
+    df_total = strip_strings(df_total)
+
+    print(f"Ready, size: {df_total.shape}")
+
+    return df_total
+
+
+def lower_memory_usage(df):
+    print(f"Memory-usage before: {df.memory_usage().sum()}")
+
+    df[df.select_dtypes(include="object").columns] = df.select_dtypes(
+        include="object").astype("category")
+
+    for old, new in [("integer", "unsigned"), ("float", "float")]:
+        for col in df.select_dtypes(include=old).columns:
+            df[col] = pd.to_numeric(df[col], downcast=new)
+
+    print(f"Memory-usage after: {df.memory_usage().sum()}")
+
+    return df
