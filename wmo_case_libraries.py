45d452e511f8234f7f8f6f0ae484e8f41ff07d07
Michiel Schakel <michiel.schakel@gmail.com>
2021-11-06 16:22:58 +0100

    .

--- wmo_case_libraries.py
+++ wmo_case_libraries.py
@@ -472,8 +472,8 @@ def biplot(score, y, coeff, labels=None, plot_pc=(0, 1)):
                      color='g',
                      ha='center',
                      va='center')
-    plt.xlim(-1, 1)
-    plt.ylim(-1, 1)
+    plt.xlim(-0.75, 0.75)
+    plt.ylim(-0.75, 0.75)
     plt.xlabel("Principle Component {}".format(plot_pc[0] + 1))
     plt.ylabel("Principle Component {}".format(plot_pc[1] + 1))
     plt.grid()
